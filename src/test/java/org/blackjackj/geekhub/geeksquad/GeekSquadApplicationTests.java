1fcb064200a702f5a6cad2fc9143933d60e56430
black-jack-j <fitisovdmtr@gmail.com>
2022-07-28 00:09:10 +0300

    geeksquad-1 Initialize GeekSquad repo
    Setup empty maven powered spring boot app

---
+++ src/test/java/org/blackjackj/geekhub/geeksquad/GeekSquadApplicationTests.java
@@ -0,0 +1,13 @@
+package org.blackjackj.geekhub.geeksquad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekSquadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
